971e79110c061069d7f4cd88a67fd42689240993
Dean Rather <deanrather@gmail.com>
2016-08-27 21:13:21 +1000

    docker-gulp works now

--- bin/docker-gulp.js
+++ bin/docker-gulp.js
@@ -1,2 +1,18 @@
 #!/usr/bin/env node
-console.log('todo: exec args in container');
+
+let cwdParts = process.cwd().split('/');
+let serviceName = cwdParts[cwdParts.length-1];
+let args = process.argv;
+args.shift();
+args.shift();
+args = args.join(' ');
+
+let command = 'docker-compose exec ' + serviceName + ' node_modules/.bin/gulp ' + args;
+
+var spawn = require('child_process').spawn;
+
+function shspawn(command) {
+   spawn('sh', ['-c', command], { stdio: 'inherit' });
+}
+
+shspawn(command);
